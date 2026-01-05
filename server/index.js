0486fdba2e8e4df113b35ba61d475040a05299d4
soumilidas03 <soudasmili@gmail.com>
2026-01-05 21:57:45 +0530

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+
+
+dotenv.config({});
+const app = express();
+
+
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+const corsOptions={
+    origin:"http://localhost:5173",
+    credintials:true
+}
+app.use(cors(corsOptions));
+
+
+
+
+const PORT= process.env.PORT||3000;
+app.listen(PORT,()=>{
+    connectDB()
+    console.log(`Server running at port : "http://localhost:${PORT}"`);
+})
