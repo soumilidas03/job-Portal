993a92cc79baaf87bcbafd257559f8eb05b0fe35
soumilidas03 <soudasmili@gmail.com>
2026-01-08 15:22:10 +0530

    Add job related files and update other files

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
 import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
+import jobRoute from "./routes/job.route.js";
 
 dotenv.config({});
 const app = express();
@@ -22,6 +23,7 @@ app.use(cors(corsOptions));
 //api
 app.use("/api/user", userRoute);
 app.use("/api/company", companyRoute);
+app.use("/api/job", jobRoute);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
